9e1846d33edba6b9658640852f8a363df49c773e
sujalraikar2004 <sujalraikar84@gmail.com>
2026-02-01 19:14:40 +0530

    all chages done

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://unified-backend.vercel.app',
         changeOrigin: true,
       },
     },
